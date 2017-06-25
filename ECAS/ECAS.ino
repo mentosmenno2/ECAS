715a980b3460c5994f688a1cfa86d99961821ef1
Menno van den Ende <mentosmenno2@hotmail.com>
2017-06-25 20:21:13 +0200

    add music player

--- ECAS/ECAS.ino
+++ ECAS/ECAS.ino
@@ -1,17 +1,39 @@
 #include <Servo.h>
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+boolean braking = false;
 
 unsigned long echo = 0;
 int ultraSoundSignal = 9; // Ultrasound signal pin
-int servoPort = 10;
 unsigned long ultrasoundValue = 0;
 int distanceThreshold = 100; //in cm
-boolean braking = false;
+int servoPort = 8;
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
 
 Servo myservo;
 
 void setup() {
+  mySoftwareSerial.begin(9600);
   Serial.begin(9600);
   pinMode(ultraSoundSignal,OUTPUT);
+
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
 }
 
 void loop() {
@@ -32,8 +54,8 @@ void brake() {
     delay(500);
     myservo.detach();
     braking = true;
+    playBrakeSound();
   }
-  playBrakeSound();
   Serial.println("Remmen!");
 }
 
@@ -49,6 +71,10 @@ void cancelBrake() {
 }
 
 void playBrakeSound() {
+  myDFPlayer.play(1);  //Play the first mp3
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
   Serial.println("Tring!");
 }
 
@@ -70,4 +96,57 @@ unsigned long ping() {
   return ultrasoundValue;
 }
 
-
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
